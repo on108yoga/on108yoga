b0db500a3099961eddfe0e1caefed71ca29a44c9
on108yoga <on108yoga@gmail.com>
2026-07-09 17:13:41 +0900

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,23 +1,93 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    console.log("calendar.js 실행");
 
     const calendarEl = document.getElementById("calendar");
 
-    console.log(calendarEl);
+
+    // 테스트용 예약 데이터
+    // 나중에 Firestore 데이터로 변경
+    const reservationData = {
+
+        "2026-07-15": 4,
+
+        "2026-07-16": 8,
+
+        "2026-07-17": 10
+
+    };
+
 
 
     const calendar = new FullCalendar.Calendar(calendarEl, {
 
+
         initialView: "dayGridMonth",
+
 
         locale: "ko",
 
-        height: "auto"
+
+        height: "auto",
+
+
+
+        events: function(fetchInfo, successCallback){
+
+
+            let events = [];
+
+
+
+            Object.keys(reservationData).forEach(function(date){
+
+
+                let count = reservationData[date];
+
+
+                let text;
+
+
+
+                if(count >= 10){
+
+                    text = "마감";
+
+                }
+
+                else{
+
+                    text = count + "/10";
+
+                }
+
+
+
+                events.push({
+
+                    title: "👥 " + text,
+
+                    start: date
+
+                });
+
+
+
+            });
+
+
+
+            successCallback(events);
+
+
+        }
+
+
 
     });
 
 
+
     calendar.render();
+
 
 });
