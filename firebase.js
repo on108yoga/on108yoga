c77d8d464f42deffda4dc719605df2d7acac4af1
on108yoga <on108yoga@gmail.com>
2026-07-10 17:34:07 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,43 +1,37 @@
 // firebase.js
 
-import { initializeApp } 
-from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
 
-import { getAuth } 
-from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+import { getAuth } from 
+"https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
 
-import { getFirestore } 
-from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+import { getFirestore } from 
+"https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 
-
+// Firebase 설정
 const firebaseConfig = {
 
-apiKey: "AIzaSyBKdaTk43A3Hp00b7gURWl7fejoAYzOkBI",
-
-authDomain: "on108-yoga.firebaseapp.com",
-
-projectId: "on108-yoga",
-
-storageBucket: "on108-yoga.firebasestorage.app",
-
-messagingSenderId: "852225153853",
-
-appId: "1:852225153853:web:e61f918ff4e0dacfac4354",
-
-measurementId: "G-7X1ELMNKXC"
+  apiKey: "여기에 Firebase API KEY",
+  authDomain: "여기에 authDomain",
+  projectId: "여기에 projectId",
+  storageBucket: "여기에 storageBucket",
+  messagingSenderId: "여기에 messagingSenderId",
+  appId: "여기에 appId"
 
 };
 
 
-
+// Firebase 초기화
 const app = initializeApp(firebaseConfig);
 
 
-
+// Authentication
 export const auth = getAuth(app);
 
 
+// Firestore
 export const db = getFirestore(app);
