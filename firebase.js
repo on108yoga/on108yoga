4f574436259f7157cd6013955be1ef03fb5111f2
on108yoga <on108yoga@gmail.com>
2026-07-10 12:11:25 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,43 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBKdaTk43A3Hp00b7gURWl7fejoAYzOkBI",
-    authDomain: "on108-yoga.firebaseapp.com",
-    projectId: "on108-yoga",
-    storageBucket: "on108-yoga.firebasestorage.app",
-    messagingSenderId: "852225153853",
-    appId: "1:852225153853:web:e61f918ff4e0dacfac4354",
-    measurementId: "G-7X1ELMNKXC"
-  };
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+
+import { getAuth } 
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+
+import { getFirestore } 
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyBKdaTk43A3Hp00b7gURWl7fejoAYzOkBI",
+
+authDomain: "on108-yoga.firebaseapp.com",
+
+projectId: "on108-yoga",
+
+storageBucket: "on108-yoga.firebasestorage.app",
+
+messagingSenderId: "852225153853",
+
+appId: "1:852225153853:web:e61f918ff4e0dacfac4354",
+
+measurementId: "G-7X1ELMNKXC"
+
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+
+
+
+export const auth = getAuth(app);
+
+
+export const db = getFirestore(app);
