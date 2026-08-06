648245f71d6405f22fd0fc0a56fa8f17228ee40f
on108yoga <on108yoga@gmail.com>
2026-08-06 11:50:11 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+// sw.js 파일 내부 상단
+const CACHE_NAME = 'on108yoga-v2'; // 기존 -v1 또는 이전 이름을 -v2, -v3 등으로 변경
+
 self.addEventListener('install', (e) => {
   console.log('Service Worker Installed');
 });
